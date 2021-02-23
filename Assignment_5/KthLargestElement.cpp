13bbf1ff4bd80a46974f98c40258320287e7eb31
Subhash Chandra <subhashchandra30720@gmail.com>
2021-02-23 11:57:31 +0530

    Assignment 5

--- Assignment_5/KthLargestElement.cpp
+++ Assignment_5/KthLargestElement.cpp
@@ -1,9 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 void siftDown(int a[], int k, int n)
 {
-
     while (2 * k <= n)
     {
         int j = 2 * k;
@@ -20,21 +19,32 @@ void siftDown(int a[], int k, int n)
     }
 }
 
+//k = 2, j = 4
+
 int main()
 {
-    int a[6] = {100, 5, 4, 3, 2, 1};
+    // your code goes here
+    int a[6] = {100, 5, 50, 4, 20, -1};
     int n = 5;
+    //a[0] = 100; ==> ignored
+    //a[1] = 50, a[2] = 20, a[3] = 4, a[4] = 5, a[5] = -1;
     for (int k = n / 2; k >= 1; k--)
     {
         siftDown(a, k, n);
     }
     int t = n;
+    cout << "Max Heap contents: " << endl;
+    for (int i = 1; i <= t; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
     while (n > 1)
     {
         swap(a[1], a[n--]);
         siftDown(a, 1, n);
     }
-    for (int i = 0; i < 6; i++)
+    for (int i = 1; i <= t; i++)
     {
         cout << a[i] << " ";
     }
